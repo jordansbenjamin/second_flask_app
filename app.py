bb8172aea38fad8618c642d286546f9535b85aaf
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-17 16:12:29 +1000

    Added educators route/endpoint

--- app.py
+++ app.py
@@ -16,3 +16,32 @@ def homepage():
 def current_time():
     time_dict = {"current time": str(datetime.now().strftime('%H:%M'))}
     return json.dumps(time_dict)
+
+@app.route("/educators")
+def educators():
+    educator_dict = {
+         "educators": [
+             {
+                 "Name": "Oliver",
+                 "Specialty": "Automated testing"
+             },
+             {
+                 "Name": "Jairo",
+                 "Specialty": "Discrete Mathematics"
+             },
+             {
+                 "Name": "Amir",
+                 "Specialty": "Web Development"
+             },
+             {
+                 "Name": "Iryna",
+                 "Specialty": "Database Engineering"
+             },
+             {
+                 "Name": "George",
+                 "Specialty": "Internet Security"
+             },
+         ]
+     }
+    return json.dumps(educator_dict)
+
